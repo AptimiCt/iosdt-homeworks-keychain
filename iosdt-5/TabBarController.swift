ef038ef998ace5e82c86830aa206cfe3074736de
Александр Востриков <aptimict11@gmail.com>
2022-11-14 00:24:41 +0300

    Инициализация проекта и добавление .gitignore

---
+++ iosdt-5/TabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarController.swift
+//  iosdt-5
+//
+//  Created by Александр Востриков on 14.11.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+
+
+}
+
